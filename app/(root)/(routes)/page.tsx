08a1ce034428a5849804547af7f7c09bcd3888d0
erickfreitas <erickfreitasbezerra@gmail.com>
2023-08-25 06:05:06 -0300

    implementando autenticação com clerk

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { UserButton } from "@clerk/nextjs"
+
 const RootPage = () => {
   return (
     <div>
-      Root Page (Protected)
+      <UserButton afterSignOutUrl="/" />
     </div>
-  );
+  )
 }
 
 export default RootPage;
